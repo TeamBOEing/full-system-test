9a0ccad2c3d5f753256de436b5fd815ff1343334
Cody Crossley <cody.crossley@eagles.oc.edu>
2015-11-05 14:08:10 -0600

    Improved comments and variable names, added debugging

--- CourseNavigateV01/CourseNavigateV01.ino
+++ CourseNavigateV01/CourseNavigateV01.ino
@@ -1,28 +1,38 @@
 /*
  * Course Navigate
- * Author: Cody Crossley
- * Date: 02 November 2015
- * Version: 1.0
+ * Author: Cody Crossley, Corbin Murrow
+ * Date: 04 November 2015
+ * Version: 1.1
  * 
- * This program will be used to test a BOEbot's ability to navigate toward a light.
+ * This program will be used to test a BOEbot's ability to navigate toward a light
+ * and avoid obstacles.
  * 
  * ======= VERSION HISTORY =======
+ * Version 1.1: Fixed light checks (higher is bright instead of lower) - CC & CM - 04 November 2015
  * Version 1.0: Created file and basic functionality - CC - 02 November 2015
  */
 
+#include <BOEbot.h>
 #include <Servo.h>
-#include "BOEbot.h"
 
-const unsigned int SPEED = 250;
+const unsigned int SPEED = 200;
 const unsigned int DRIVE_TIME = 500;
-const unsigned int TURN_TIME = 50;
-const unsigned int TURN_TIME_2 = 250;
-const unsigned int LIGHT_READ_SINGLE = 800;
-const unsigned int LIGHT_READ_BOTH = 1600;
-const float LIGHT_RATIO_LEFT = 0.7;
-const float LIGHT_RATIO_RIGHT = 1.3;
+const unsigned int TURN_TIME_LIGHT = 75;
+const unsigned int TURN_TIME_AVOID = 250;
+const unsigned int LIGHT_READ_SINGLE = 875;
+const unsigned int LIGHT_READ_BOTH = LIGHT_READ_SINGLE * 1.85;
+const float LIGHT_RATIO_LEFT = 1.05;
+const float LIGHT_RATIO_RIGHT = 0.95;
 
+const bool DEBUG_MOTORS = false;     // When true display motor info
+const bool DEBUG_LIGHTS = false;     // When true display light info
+const bool DEBUG_OBSTACLE = false;   // When true display obstacle info
+
+// Drives BOEbot forward
 void driveForw(unsigned int spd, unsigned int time_length) {
+  if (DEBUG_MOTORS)
+    Serial.println("Driving forward.");
+  
   turnLeftMotorForward(spd);
   turnRightMotorForward(spd);
   delay(time_length);
@@ -30,7 +40,11 @@ void driveForw(unsigned int spd, unsigned int time_length) {
   stopRightMotor();
 }
 
+// Drives BOEbot backward
 void driveBack(unsigned int spd, unsigned int time_length) {
+  if (DEBUG_MOTORS)
+    Serial.println("Driving backward.");
+  
   turnLeftMotorBackward(spd);
   turnRightMotorBackward(spd);
   delay(time_length);
@@ -38,7 +52,11 @@ void driveBack(unsigned int spd, unsigned int time_length) {
   stopRightMotor();
 }
 
+// Turns BOEbot left
 void turnLeft(unsigned int spd, unsigned int time_length) {
+  if (DEBUG_MOTORS)
+    Serial.println("Turning left.");
+  
   turnLeftMotorBackward(spd);
   turnRightMotorForward(spd);
   delay(time_length);
@@ -46,7 +64,11 @@ void turnLeft(unsigned int spd, unsigned int time_length) {
   stopRightMotor();
 }
 
+// Turns BOEbot right
 void turnRight(unsigned int spd, unsigned int time_length) {
+  if (DEBUG_MOTORS)
+    Serial.println("Turning right.");
+  
   turnLeftMotorForward(spd);
   turnRightMotorBackward(spd);
   delay(time_length);
@@ -59,54 +81,88 @@ void setup() {
 }
 
 void loop() {
-  bool continueBot = true;
-  float leftLight = 1000;
-  float rightLight = 1000;
+  bool continueBot = true;    // Run BOEbot until near light
+  float leftLight;            // Store left light reading for comparison
+  float rightLight;           // Store right light reading for comparison
 
   // While not near the light, navigate around
   // obstacles and toward the light
   while (continueBot) {
+    if (DEBUG_MOTORS || DEBUG_LIGHTS || DEBUG_OBSTACLE)
+      Serial.println("\n ***** New Loop *****\n");
+    
     // Obstacle detection
     if( leftObstacle() && rightObstacle() ) {
+      if (DEBUG_OBSTACLE)
+        Serial.println("Obstacle in front.");
+      
       driveBack(SPEED, DRIVE_TIME);
-      turnLeft(SPEED, TURN_TIME_2);
+      turnLeft(SPEED, TURN_TIME_AVOID);
     }
     else if ( leftObstacle() ) {
+      if (DEBUG_OBSTACLE)
+        Serial.println("Obstacle to left.");
+      
       driveBack(SPEED, DRIVE_TIME);
-      turnRight(SPEED, TURN_TIME_2);
+      turnRight(SPEED, TURN_TIME_AVOID);
     }
     else if ( rightObstacle() ) {
+      if (DEBUG_OBSTACLE)
+        Serial.println("Obstacle to right.");
+      
       driveBack(SPEED, DRIVE_TIME);
-      turnLeft(SPEED, TURN_TIME_2);
+      turnLeft(SPEED, TURN_TIME_AVOID);
     } // End of obstacle detection
     
-    driveForw(SPEED, DRIVE_TIME);
+    driveForw(SPEED, DRIVE_TIME);   // Always move forward
     
-    // Light detection
+    // Navigate toward light if no new obstacles in front of bot
     if( !leftObstacle() && !rightObstacle() ) {
-      leftLight = getLeftLight();
-      rightLight = getRightLight();
+      leftLight = getLeftLight() + 1;
+      rightLight = getRightLight() + 1;
+
+      if (DEBUG_LIGHTS) {
+        Serial.print("Left Light: " + (String)leftLight);
+        Serial.println("\tRight Light: " + (String)rightLight);
+      }
       
+      // If close to light indicate finished with course
       if ( (leftLight > LIGHT_READ_SINGLE) ||
            (rightLight > LIGHT_READ_SINGLE) ||
-           ( (leftLight+rightLight) > LIGHT_READ_BOTH) ) {
+           ( (leftLight + rightLight) > LIGHT_READ_BOTH) ) {
         continueBot = false;
+
+        if (DEBUG_LIGHTS)
+          Serial.println("Arrived at light.");
       }
+      // If not close to light, turn toward or move toward light
       else {
+        // If not pointing toward light, turn toward without moving forward
         if ( (leftLight / rightLight) > LIGHT_RATIO_LEFT) {
-          turnLeft(SPEED, TURN_TIME);
+          if (DEBUG_LIGHTS)
+            Serial.println("Light to left.");
+          
+          turnLeft(SPEED, TURN_TIME_LIGHT);
         }
         else if ( (leftLight / rightLight) < LIGHT_RATIO_RIGHT) {
-          turnRight(SPEED, TURN_TIME);
+          if (DEBUG_LIGHTS)
+            Serial.println("Light to right.");
+            
+          turnRight(SPEED, TURN_TIME_LIGHT);
         }
+        // Facing light
         else {
-          driveForw(SPEED, TURN_TIME);
+          if (DEBUG_LIGHTS)
+            Serial.println("Light in front.");
         }
       }
     } // End of light detection
     
   } // End of move bot toward light
 
+  Serial.println("\nCourse Completed!\n");
+  
+  // Generic sound to indicate course completed
   playSound(300, 1000);
   halt();
 }
